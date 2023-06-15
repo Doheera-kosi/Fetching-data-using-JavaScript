ffae2e2c1e38bcac18f9006e52e9aaabd74b7e8a
Doheera-kosi <kupourevans99@gmail.com>
2023-06-15 14:52:25 +0000

    Modify settings.py

--- fetchdataproject/settings.py
+++ fetchdataproject/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'blog_db',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': 'root', 
+        'USER': 'root',
         'PASSWORD': 'super',
     }
 }
